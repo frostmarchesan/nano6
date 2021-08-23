e2ff6ec7b2ede69ef0be7764980c15c8c557ac5b
frostmarchesan <55881881+frostmarchesan@users.noreply.github.com>
2021-08-23 15:02:31 -0300

    Initial Commit

---
+++ nano6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nano6
+//
+//  Created by marcelo frost marchesan on 23/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nano6/nano6App.swift
@@ -0,0 +1,17 @@
+//
+//  nano6App.swift
+//  nano6
+//
+//  Created by marcelo frost marchesan on 23/08/21.
+//
+
+import SwiftUI
+
+@main
+struct nano6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
